cec81d579e5940a4a2fbb221a5795fa5ee3affec
cabellojaysonnew-max <cabello.jayson.new@gmail.com>
2026-04-16 10:40:34 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://ytfpiyfapvybihlngxks.supabase.co";
-const SUPABASE_KEY = "sb_secret_YNX-IJjveulE3N3gT_yBdA_6PzxoKdB";
+const SUPABASE_URL = "https://ytfpiyfapvybihlngxks.supabase.co"; // Assuming this URL is still correct
+const SUPABASE_KEY = "sb_publishable_poSZUQ9HI4wcY9poEo5b1w_Z-pAJbKo"; // Replaced with your new key
 
 // By attaching it to 'window', we force the browser to make it available globally to your HTML file.
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
